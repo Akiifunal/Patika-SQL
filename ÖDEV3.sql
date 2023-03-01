da0da388560567c66a5cf2d469902652267bb317
Akiifunal <111228884+Akiifunal@users.noreply.github.com>
2023-03-01 15:55:18 +0300

    Update ÖDEV3.sql

--- ÖDEV3.sql
+++ ÖDEV3.sql
@@ -1,1 +1,11 @@
+SELECT * FROM country
+WHERE country LIKE 'A%a'
 
+SELECT * FROM country
+WHERE country LENGTH(country) >= 6 LIKE '%n'
+
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%'
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
