1c43a79dcdfdbcc1d6fbc8311637b19ba250c863
Akiifunal <111228884+Akiifunal@users.noreply.github.com>
2023-03-08 17:14:51 +0300

    Update ödev11.sql

--- ödev11.sql
+++ ödev11.sql
@@ -1,28 +1,52 @@
 1.actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
-(SELECT first_name FROM actor)
+(
+  SELECT first_name FROM actor
+)
 UNION 
-(SELECT first_name FROM customer)
+(
+  SELECT first_name FROM customer
+)
 
 2.actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
-(SELECT first_name FROM actor)
+(
+  SELECT first_name FROM actor
+)
 INTERSECT
-(SELECT first_name FROM customer)
+(
+  SELECT first_name FROM customer
+)
 
 3.actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
 
-(SELECT first_name FROM actor)
+(
+  SELECT first_name FROM actor
+)
 EXCEPT
-(SELECT first_name FROM customer)
+(
+  SELECT first_name FROM customer
+)
 
 
-4.(SELECT first_name FROM actor)
+4.(
+  SELECT first_name FROM actor
+)
 UNION ALL
-(SELECT first_name FROM customer)
+(
+  SELECT first_name FROM customer
+)
 
-(SELECT first_name FROM actor)
+(
+  SELECT first_name FROM actor
+)
 INTERSECT ALL
-(SELECT first_name FROM customer)
+(
+  SELECT first_name FROM customer
+)
 
-(SELECT first_name FROM actor)
+(
+  SELECT first_name FROM actor
+)
 EXCEPT
-(SELECT first_name FROM customer)
+(
+  SELECT first_name FROM customer
+)
