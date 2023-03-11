a6addefe3a20671a8f8cccb5a8a825d2a8f9e4f8
Akiifunal <111228884+Akiifunal@users.noreply.github.com>
2023-03-11 16:34:58 +0300

    Update ödev12.sql

--- ödev12.sql
+++ ödev12.sql
@@ -14,8 +14,11 @@ WHERE rental_rate =
 
 
 film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
-SELECT title,rental_rate,replacement_cost FROM film WHERE rental_rate = 
+SELECT title,rental_rate,replacement_cost FROM film WHERE rental_rate =
 (SELECT MIN(rental_rate) FROM film) AND replacement_cost =  (SELECT MIN(replacement_cost) FROM film);
 
 
 payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer_id, COUNT(payment_id) FROM payment 
+ GROUP BY customer_id
+ ORDER BY COUNT DESC;
