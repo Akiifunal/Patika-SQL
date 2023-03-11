61c46af0be74fb6751c5196d0925a8a54322b70d
Akiifunal <111228884+Akiifunal@users.noreply.github.com>
2023-03-11 16:37:02 +0300

    Update ödev12.sql

--- ödev12.sql
+++ ödev12.sql
@@ -1,7 +1,7 @@
 film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
 SELECT COUNT (*), FROM film
-WHERE length >
-( SELECT AVG(title) 
+WHERE length > ALL
+( SELECT AVG(length) 
 FROM film 
 )
 
@@ -15,7 +15,8 @@ WHERE rental_rate =
 
 film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
 SELECT title,rental_rate,replacement_cost FROM film WHERE rental_rate =
-(SELECT MIN(rental_rate) FROM film) AND replacement_cost =  (SELECT MIN(replacement_cost) FROM film);
+(SELECT MIN(rental_rate) FROM film) AND replacement_cost =  (SELECT MIN(replacement_cost) FROM film) 
+ORDER BY title;
 
 
 payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
