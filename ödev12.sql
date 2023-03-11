48aa0ed3d69c945748fa01c2a43975c62e23dc07
Akiifunal <111228884+Akiifunal@users.noreply.github.com>
2023-03-11 16:43:24 +0300

    Update ödev12.sql

--- ödev12.sql
+++ ödev12.sql
@@ -1,5 +1,5 @@
 film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
-SELECT COUNT (*), FROM film
+SELECT COUNT (*) FROM film
 WHERE length > ALL
 ( SELECT AVG(length) 
 FROM film 
